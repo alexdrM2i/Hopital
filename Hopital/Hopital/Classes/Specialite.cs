92550a8fcf6ce1df856e7a716f492199f2ce5697
abdel85 <bouhellal.a@gmail.com>
2019-05-15 14:07:56 +0200

    v

---
+++ Hopital/Hopital/Classes/Specialite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hopital.Classes
+{
+    public class Specialite
+    {
+        private int id;
+        private string specialiteM;
+
+        public int Id { get => id; set => id = value; }
+        public string SpecialiteM { get => specialiteM; set => specialiteM = value; }
+
+        public Specialite()
+        {
+            
+        }
+
+    }
+
+}
