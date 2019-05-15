86a67ec57b96c4d361b238ddd50333f360393de8
Fabien <fabien.greaux@orange.fr>
2019-05-15 11:51:59 +0100

    classe connection et database

---
+++ Hopital/Hopital/Classes/DataBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hopital.Classes
+{
+    public class DataBase
+    {
+        private static DataBase _instance = null;
+        private static object _lock = new object();
+
+        public static DataBase Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DataBase();
+                    }
+                    return _instance;
+                }
+            }
+        }
+        private DataBase()
+        {
+
+        }
+    }
+}
