232de0e04435d0f39929994ec210dec7bdfce225
olivier-Desmalades <o.desmalades@hotmail.fr>
2019-05-15 10:44:07 +0200

    modification du nom de la class message erreur en class messages

---
+++ Hopital/Hopital/Classes/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hopital.Classes
+{
+    public static class Messages
+    {
+        public static string NotANumber = "Veuillez saisir un chiffre";
+        public static string Choix = "Veuillez indiquer votre choix ";
+        public static string NotAString = "Veuillez indiquer une chaine de caractère validre ";
+    }
+}
