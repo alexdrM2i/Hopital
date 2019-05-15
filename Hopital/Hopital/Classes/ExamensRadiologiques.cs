75ff529ce5c0e30bf40e598910726cc739229637
abdel85 <bouhellal.a@gmail.com>
2019-05-15 10:54:12 +0200

    fin classes

---
+++ Hopital/Hopital/Classes/ExamensRadiologiques.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hopital.Classes
+{
+    public  class ExamensRadiologiques
+    {
+        private int id;
+        private int resultat;
+        private string designation;
+
+        public int Id { get => id; set => id = value; }
+        public int Resultat { get => resultat; set => resultat = value; }
+        public string Designation { get => designation; set => designation = value; }
+
+        public ExamensRadiologiques()
+        {
+            
+        }
+    }
+}
