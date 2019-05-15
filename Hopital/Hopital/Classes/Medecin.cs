8097e48246dbba9cadc6868394203e96ccd96e0f
abdel85 <bouhellal.a@gmail.com>
2019-05-15 10:32:46 +0200

    ajoutes classes

---
+++ Hopital/Hopital/Classes/Medecin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hopital.Classes
+{
+    public class Medecin
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private string tel;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Tel { get => tel; set => tel = value; }
+
+        public Medecin()
+        {
+            
+        }
+    }
+    
+}
