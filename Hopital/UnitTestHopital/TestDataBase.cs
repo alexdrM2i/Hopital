06d074e74ee6f5f8f38d78ace2617153ca314adb
Fabien <fabien.greaux@orange.fr>
2019-05-28 16:31:14 +0100

    local

---
+++ Hopital/UnitTestHopital/TestDataBase.cs
@@ -0,0 +1,19 @@
+﻿using Hopital.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestHopital
+{
+    [TestClass]
+    public class TestDataBase
+    {
+        [TestMethod]
+        public void Add_Patient_void()
+        {
+            Patient p = new Patient();
+            
+        }
+    }
+}
